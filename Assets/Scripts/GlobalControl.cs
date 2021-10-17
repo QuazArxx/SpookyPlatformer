0c71f3e6aa4421b310e2589c25f3ab08e087bdf7
quazarxx <mysubs666@gmail.com>
2021-10-17 11:09:34 -0400

    Added Game Master

---
+++ Assets/Scripts/GlobalControl.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalControl : MonoBehaviour
+{
+    public static GlobalControl Instance;
+
+    public float lives;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
